e464f5d355402717e21d2169d302108e9c119363
Jane Vandegriff <jane@vandymail.com>
2026-05-09 00:52:07 -0400

    use envsubst

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 set -e # exit when any error happens
+set -a # automatically export all variables
 source .env.prod
+set +a
 set -x # show commands running
 
 if [[ -z "$KUBECONFIG" ]]; then
   echo '$KUBECONFIG not specified in .env.prod, please add it'
   exit 1
 fi
-export KUBECONFIG
 ns=status #namespace
 
 # kubectl -n $ns delete deploy gatus
 
 set +x # hide commands temporarily for secrets
 cat status-page.yaml | \
-    sed "s,{{MATRIX_HOMESERVER}},${MATRIX_HOMESERVER},g" | \
-    sed "s,{{MATRIX_ACCESS_TOKEN}},${MATRIX_ACCESS_TOKEN},g" | \
-    sed "s,{{MATRIX_ROOM_ID}},${MATRIX_ROOM_ID},g" | \
+    envsubst '$MATRIX_HOMESERVER,$MATRIX_ROOM_ID,$MATRIX_ACCESS_TOKEN' | \
     kubectl -n $ns apply -f -
 set -x #show commands again
 
